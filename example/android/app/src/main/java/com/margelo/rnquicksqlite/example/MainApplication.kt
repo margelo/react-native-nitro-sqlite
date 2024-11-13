a1b14fabdc3befd49e7259cfcc348151c5b93014
Christoph Pader <chris@margelo.com>
2024-11-13 11:07:22 +0100

    simplify CMakeLists

--- example/android/app/src/main/java/com/margelo/rnquicksqlite/example/MainApplication.kt
+++ example/android/app/src/main/java/com/margelo/rnquicksqlite/example/MainApplication.kt
@@ -11,7 +11,6 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
-import com.margelo.rnquicksqlite.RNQuickSQLitePackage
 
 class MainApplication : Application(), ReactApplication {
 
