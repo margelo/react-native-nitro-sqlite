67b4af3a7c0301f5056964cb6499c708ffb458ae
Christoph Pader <chris@margelo.com>
2024-11-27 13:40:39 +0100

    fix: implement std::runtime_error instead of std::exception

--- package/cpp/NitroSQLiteException.hpp
+++ package/cpp/NitroSQLiteException.hpp
@@ -28,22 +28,15 @@ inline std::string typeToString(NitroSQLiteExceptionType type) {
   return exceptionTypeStrings[type];
 }
 
-class NitroSQLiteException : public std::exception {
+class NitroSQLiteException : public std::runtime_error {
 public:
   explicit NitroSQLiteException(const char* message): NitroSQLiteException(NitroSQLiteExceptionType::UnknownError, message) {}
   explicit NitroSQLiteException(const std::string& message): NitroSQLiteException(NitroSQLiteExceptionType::UnknownError, message) {}
   NitroSQLiteException(const NitroSQLiteExceptionType& type, const char* message) : NitroSQLiteException(type, std::string(message)) {}
   NitroSQLiteException(const NitroSQLiteExceptionType& type, const std::string& message)
-    : _exceptionString("[react-native-nitro-sqlite] " + typeToString(type) + ": " + message) {}
-
-private:
-  const std::string _exceptionString;
+    : std::runtime_error("[react-native-nitro-sqlite] " + typeToString(type) + ": " + message) {}
 
 public:
-  [[nodiscard]] const char* what() const noexcept override {
-    return this->_exceptionString.c_str();
-  }
-
   static NitroSQLiteException DatabaseNotOpen(const std::string& dbName) {
     return NitroSQLiteException(NitroSQLiteExceptionType::UnableToAttachToDatabase, dbName + " is not open");
   }
