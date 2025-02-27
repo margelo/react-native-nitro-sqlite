a948b75da92b58b3693af13fb8729620b3acb582
Christoph Pader <chris@margelo.com>
2025-02-27 21:02:25 +0100

    fix: move files to kotlin folder

--- package/android/src/main/kotlin/com/margelo/rnnitrosqlite/HybridNitroSQLiteOnLoad.kt
+++ package/android/src/main/kotlin/com/margelo/rnnitrosqlite/HybridNitroSQLiteOnLoad.kt
@@ -1,9 +1,16 @@
 package com.margelo.rnnitrosqlite
 
+import androidx.annotation.Keep
+import com.facebook.proguard.annotations.DoNotStrip
 import com.margelo.nitro.NitroModules
+import com.margelo.nitro.rnnitrosqlite.HybridNitroSQLiteOnLoadSpec
 import com.margelo.rnnitrosqlite.DocPathSetter
 
+@Keep
+@DoNotStrip
 class HybridNitroSQLiteOnLoad : HybridNitroSQLiteOnLoadSpec() {
+  @Keep
+  @DoNotStrip
   override fun init() {
     DocPathSetter.setDocPath(NitroModules.context)
   }
