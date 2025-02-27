1d3c1beb186d065be2d161791049a4a43cefcd81
Christoph Pader <chris@margelo.com>
2025-02-27 21:14:36 +0100

    fix: move package kotlin file

--- package/android/src/main/kotlin/com/margelo/rnnitrosqlite/RNNitroSQLitePackage.kt
+++ package/android/src/main/kotlin/com/margelo/rnnitrosqlite/RNNitroSQLitePackage.kt
@@ -1,16 +1,23 @@
 package com.margelo.rnnitrosqlite
 
+import java.util.HashMap;
+import java.util.function.Supplier;
 import com.facebook.react.TurboReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.model.ReactModuleInfo
 import com.facebook.react.module.model.ReactModuleInfoProvider
+import com.margelo.nitro.core.HybridObject;
+
 
 class RNNitroSQLitePackage : TurboReactPackage() {
+    @Nullable
+    @Override
     override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
         return null
     }
 
+    @Override
     override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
         return {
             mapOf()
