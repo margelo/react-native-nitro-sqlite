dfdaec80d4e9a062878062c8315511babc4116c5
Christoph Pader <chris@margelo.com>
2025-11-03 21:36:33 +0000

    fix: eslint errors

--- example/src/initGlobals.ts
+++ example/src/initGlobals.ts
@@ -1,11 +1,12 @@
 import { Buffer as CraftzdogBuffer } from '@craftzdog/react-native-buffer'
 
 declare global {
+  // eslint-disable-next-line no-var
   var Buffer: typeof CraftzdogBuffer
 }
 
 if (!globalThis.process) {
-  // @ts-expect-error
+  // @ts-expect-error - if process is not defined, we need to set it to an empty object
   globalThis.process = {}
 }
 
