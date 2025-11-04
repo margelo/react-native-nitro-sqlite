20914c5c9148a4f328a4c6aba7a283994e452395
Christoph Pader <chris@margelo.com>
2025-11-04 12:18:37 +0000

    fix: Remove compiler warnings (#225)
    * chore: add .ruby-version file and update version in GH workflow
    
    * fix: add `pragma once` to `logs.hpp`
    
    * fix: remove unused variables
    
    * chore: rename LOG_TAG
    
    * remove LOG_TAG from CMakeLists.txt

--- package/cpp/logs.hpp
+++ package/cpp/logs.hpp
@@ -1,37 +1,39 @@
+#pragma once
+
+#define NITRO_SQLITE_LOG_TAG "react-native-nitro-sqlite"
+
 #ifdef ANDROID
 // LOGS ANDROID
 #include <android/log.h>
-#define LOG_TAG "react-native-nitro-sqlite"
-#define LOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
-#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, NITRO_SQLITE_LOG_TAG, __VA_ARGS__)
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, NITRO_SQLITE_LOG_TAG, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, NITRO_SQLITE_LOG_TAG, __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, NITRO_SQLITE_LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, NITRO_SQLITE_LOG_TAG, __VA_ARGS__)
 #define LOGSIMPLE(...)
 #else
 // LOGS NO ANDROID
 #include <stdio.h>
-#define LOG_TAG "react-native-nitro-sqlite"
 #define LOGV(...)                                                                                                                          \
   printf("  ");                                                                                                                            \
   printf(__VA_ARGS__);                                                                                                                     \
-  printf("\t -  <%s> \n", LOG_TAG);
+  printf("\t -  <%s> \n", NITRO_SQLITE_LOG_TAG);
 #define LOGD(...)                                                                                                                          \
   printf("  ");                                                                                                                            \
   printf(__VA_ARGS__);                                                                                                                     \
-  printf("\t -  <%s> \n", LOG_TAG);
+  printf("\t -  <%s> \n", NITRO_SQLITE_LOG_TAG);
 #define LOGI(...)                                                                                                                          \
   printf("  ");                                                                                                                            \
   printf(__VA_ARGS__);                                                                                                                     \
-  printf("\t -  <%s> \n", LOG_TAG);
+  printf("\t -  <%s> \n", NITRO_SQLITE_LOG_TAG);
 #define LOGW(...)                                                                                                                          \
   printf("  * Warning: ");                                                                                                                 \
   printf(__VA_ARGS__);                                                                                                                     \
-  printf("\t -  <%s> \n", LOG_TAG);
+  printf("\t -  <%s> \n", NITRO_SQLITE_LOG_TAG);
 #define LOGE(...)                                                                                                                          \
   printf("  *** Error:  ");                                                                                                                \
   printf(__VA_ARGS__);                                                                                                                     \
-  printf("\t -  <%s> \n", LOG_TAG);
+  printf("\t -  <%s> \n", NITRO_SQLITE_LOG_TAG);
 #define LOGSIMPLE(...)                                                                                                                     \
   printf(" ");                                                                                                                             \
   printf(__VA_ARGS__);
