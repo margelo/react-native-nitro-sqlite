8df57b9dec13cf54050c9c0321c745200dc38658
Christoph Pader <chris@margelo.com>
2025-11-05 23:29:58 +0000

    feat: forward native exception

--- package/src/NitroSQLiteError.ts
+++ package/src/NitroSQLiteError.ts
@@ -1,15 +1,25 @@
+const NATIVE_NITRO_SQLITE_EXCEPTION_PREFIX = '[NitroSQLiteException] ' as const
 const NITRO_SQLITE_ERROR_NAME = 'NitroSQLiteError' as const
-const NITRO_SQLITE_ERROR_PREFIX = '[NitroSQLite] ' as const
+const getNitroSQLiteErrorPrefix = (isNativeNitroSQLiteException: boolean) =>
+  isNativeNitroSQLiteException ? '[NitroSQLite (Native/C++)]' : '[NitroSQLite] '
+
+type NitroSQLiteErrorOptions = ErrorOptions & {
+  isNativeNitroSQLiteException?: boolean
+}
 
 /**
  * Custom error class for NitroSQLite operations
  * Extends the native Error class with proper prototype chain and error handling
  */
 export default class NitroSQLiteError extends Error {
-  constructor(message: string, options?: ErrorOptions) {
-    super(message, options)
+  constructor(message: string, options?: NitroSQLiteErrorOptions) {
+    const { isNativeNitroSQLiteException = false, ...restOptions } =
+      options ?? {}
+
+    super(message, restOptions)
     this.name = NITRO_SQLITE_ERROR_NAME
-    this.message = NITRO_SQLITE_ERROR_PREFIX + message
+    this.message =
+      getNitroSQLiteErrorPrefix(isNativeNitroSQLiteException) + message
 
     // Maintains proper prototype chain for instanceof checks
     Object.setPrototypeOf(this, NitroSQLiteError.prototype)
@@ -25,6 +35,13 @@ export default class NitroSQLiteError extends Error {
     }
 
     if (error instanceof Error) {
+      if (error.message.includes(NATIVE_NITRO_SQLITE_EXCEPTION_PREFIX)) {
+        return new NitroSQLiteError(error.message, {
+          cause: error.cause,
+          isNativeNitroSQLiteException: true,
+        })
+      }
+
       const nitroSQLiteError = new NitroSQLiteError(error.message, {
         cause: error.cause,
       })
@@ -36,7 +53,11 @@ export default class NitroSQLiteError extends Error {
     }
 
     if (typeof error === 'string') {
-      return new NitroSQLiteError(error)
+      return new NitroSQLiteError(error, {
+        isNativeNitroSQLiteException: error.includes(
+          NATIVE_NITRO_SQLITE_EXCEPTION_PREFIX,
+        ),
+      })
     }
 
     return new NitroSQLiteError('Unknown error occurred', {
