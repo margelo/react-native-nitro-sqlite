18d16f23b213c4a4a0d2121f3d679bdd33acfaf3
Christoph Pader <chris@margelo.com>
2026-02-04 17:11:10 +0100

    fix: still need `buildJsQueryResult`

--- package/src/operations/execute.ts
+++ package/src/operations/execute.ts
@@ -10,8 +10,7 @@ export function execute<Row extends QueryResultRow = never>(
 ): QueryResult<Row> {
   try {
     const result = HybridNitroSQLite.execute(dbName, query, params)
-
-    return result as NitroSQLiteQueryResult & QueryResult<Row>
+    return buildJsQueryResult<Row>(result)
   } catch (error) {
     throw NitroSQLiteError.fromError(error)
   }
@@ -24,8 +23,25 @@ export async function executeAsync<Row extends QueryResultRow = never>(
 ): Promise<QueryResult<Row>> {
   try {
     const result = await HybridNitroSQLite.executeAsync(dbName, query, params)
-    return result as NitroSQLiteQueryResult & QueryResult<Row>
+    return buildJsQueryResult<Row>(result)
   } catch (error) {
     throw NitroSQLiteError.fromError(error)
   }
+}
+
+function buildJsQueryResult<Row extends QueryResultRow = never>(
+  result: NitroSQLiteQueryResult,
+): QueryResult<Row> {
+  const data = result.results as Row[]
+
+  return {
+    ...result,
+    insertId: result.insertId,
+    rowsAffected: result.rowsAffected,
+    rows: {
+      _array: data,
+      length: data.length,
+      item: (idx: number) => data[idx],
+    },
+  } as QueryResult<Row>
 }
