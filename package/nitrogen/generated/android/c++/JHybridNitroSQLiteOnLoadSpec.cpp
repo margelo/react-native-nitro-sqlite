e7b27b811aacc103cb9a9f96f9788bcd598d5e9a
DaveyEke <daveydenco@gmail.com>
2026-03-06 13:48:19 +0100

    chore: upgrade Nitro to 0.35.0

--- package/nitrogen/generated/android/c++/JHybridNitroSQLiteOnLoadSpec.cpp
+++ package/nitrogen/generated/android/c++/JHybridNitroSQLiteOnLoadSpec.cpp
@@ -13,37 +13,31 @@
 
 namespace margelo::nitro::rnnitrosqlite {
 
-  jni::local_ref<JHybridNitroSQLiteOnLoadSpec::jhybriddata> JHybridNitroSQLiteOnLoadSpec::initHybrid(jni::alias_ref<jhybridobject> jThis) {
+  std::shared_ptr<JHybridNitroSQLiteOnLoadSpec> JHybridNitroSQLiteOnLoadSpec::JavaPart::getJHybridNitroSQLiteOnLoadSpec() {
+    auto hybridObject = JHybridObject::JavaPart::getJHybridObject();
+    auto castHybridObject = std::dynamic_pointer_cast<JHybridNitroSQLiteOnLoadSpec>(hybridObject);
+    if (castHybridObject == nullptr) [[unlikely]] {
+      throw std::runtime_error("Failed to downcast JHybridObject to JHybridNitroSQLiteOnLoadSpec!");
+    }
+    return castHybridObject;
+  }
+
+  jni::local_ref<JHybridNitroSQLiteOnLoadSpec::CxxPart::jhybriddata> JHybridNitroSQLiteOnLoadSpec::CxxPart::initHybrid(jni::alias_ref<jhybridobject> jThis) {
     return makeCxxInstance(jThis);
   }
 
-  void JHybridNitroSQLiteOnLoadSpec::registerNatives() {
-    registerHybrid({
-      makeNativeMethod("initHybrid", JHybridNitroSQLiteOnLoadSpec::initHybrid),
-    });
-  }
-
-  size_t JHybridNitroSQLiteOnLoadSpec::getExternalMemorySize() noexcept {
-    static const auto method = javaClassStatic()->getMethod<jlong()>("getMemorySize");
-    return method(_javaPart);
-  }
-
-  bool JHybridNitroSQLiteOnLoadSpec::equals(const std::shared_ptr<HybridObject>& other) {
-    if (auto otherCast = std::dynamic_pointer_cast<JHybridNitroSQLiteOnLoadSpec>(other)) {
-      return _javaPart == otherCast->_javaPart;
+  std::shared_ptr<JHybridObject> JHybridNitroSQLiteOnLoadSpec::CxxPart::createHybridObject(const jni::local_ref<JHybridObject::JavaPart>& javaPart) {
+    auto castJavaPart = jni::dynamic_ref_cast<JHybridNitroSQLiteOnLoadSpec::JavaPart>(javaPart);
+    if (castJavaPart == nullptr) [[unlikely]] {
+      throw std::runtime_error("Failed to cast JHybridObject::JavaPart to JHybridNitroSQLiteOnLoadSpec::JavaPart!");
     }
-    return false;
+    return std::make_shared<JHybridNitroSQLiteOnLoadSpec>(castJavaPart);
   }
 
-  void JHybridNitroSQLiteOnLoadSpec::dispose() noexcept {
-    static const auto method = javaClassStatic()->getMethod<void()>("dispose");
-    method(_javaPart);
-  }
-
-  std::string JHybridNitroSQLiteOnLoadSpec::toString() {
-    static const auto method = javaClassStatic()->getMethod<jni::JString()>("toString");
-    auto javaString = method(_javaPart);
-    return javaString->toStdString();
+  void JHybridNitroSQLiteOnLoadSpec::CxxPart::registerNatives() {
+    registerHybrid({
+      makeNativeMethod("initHybrid", JHybridNitroSQLiteOnLoadSpec::CxxPart::initHybrid),
+    });
   }
 
   // Properties
@@ -51,7 +45,7 @@ namespace margelo::nitro::rnnitrosqlite {
 
   // Methods
   void JHybridNitroSQLiteOnLoadSpec::init() {
-    static const auto method = javaClassStatic()->getMethod<void()>("init");
+    static const auto method = _javaPart->javaClassStatic()->getMethod<void()>("init");
     method(_javaPart);
   }
 
