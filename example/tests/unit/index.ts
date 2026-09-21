326ebb3ad3593e9c2ef2d1c41fb7d67dc3cc09c1
Christoph Pader <chris@margelo.com>
2026-09-21 13:21:22 +0200

    fix(example): reset persisted benchmark schema (#347)
    Co-authored-by: Samuel Scheit <github@samuelscheit.com>

--- example/tests/unit/index.ts
+++ example/tests/unit/index.ts
@@ -6,6 +6,7 @@ import registerExecuteBatchUnitTests from './specs/operations/executeBatch.spec'
 import registerTypeORMUnitTestsSpecs from './specs/typeorm.spec'
 import registerDatabaseQueueUnitTests from './specs/DatabaseQueue.spec'
 import registerSqliteVecUnitTestsSpecs from './specs/sqlite-vec.spec'
+import registerBenchmarkDatabaseUnitTests from './specs/benchmarkDatabase.spec'
 
 export function registerUnitTests() {
   beforeEach(setupTestDb)
@@ -14,6 +15,7 @@ export function registerUnitTests() {
   describe('operations/transaction.spec.ts', registerTransactionUnitTests)
   describe('operations/executeBatch.spec.ts', registerExecuteBatchUnitTests)
   describe('DatabaseQueue.spec.ts', registerDatabaseQueueUnitTests)
+  describe('benchmarkDatabase.spec.ts', registerBenchmarkDatabaseUnitTests)
 }
 
 export function registerTypeORMUnitTests() {
